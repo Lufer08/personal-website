3925be9a4972ac82d6f58ed944960780e772c579
Lufer08 <luferreyes08@gmail.com>
2025-09-23 15:48:02 -0500

    fix: add basename for React Router in GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './features/pages/App'
 import InfoProjectPage from './features/pages/InfoProjectPage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/info-project" element={<InfoProjectPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
